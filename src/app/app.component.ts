cab347203c094302b1e12b20e912b820432c48da
Islom Abdurahmonov <islomabdurakhmonov8@gmail.com>
2023-12-05 23:53:21 +0500

    search bar adde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
