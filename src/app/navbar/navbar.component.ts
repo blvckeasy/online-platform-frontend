28c4a6267f2f3c8ef3d1650abce311d616ebb7cc
Islom Abdurahmonov <islomabdurakhmonov8@gmail.com>
2023-12-10 23:13:16 +0500

    styles updated and navbar signin button updated

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  	selector: 'app-navbar',
+  	standalone: true,
+  	imports: [
+		ButtonComponent,
+	],
+  	templateUrl: './navbar.component.html',
+  	styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
