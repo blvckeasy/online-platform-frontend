805e6842958798841d0faf8cbb1390e70134fe65
Islom Abdurahmonov <islomabdurakhmonov8@gmail.com>
2023-12-15 20:19:48 +0500

    footer updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons'
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
     imports: [
-      FontAwesomeModule,
-    ],
+      	FontAwesomeModule,
+		MatIconModule,
+	],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css'
 })
