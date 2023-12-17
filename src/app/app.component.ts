9be5f7d5885108a4a561de31e915ecd4c9dda5ff
Islom Abdurahmonov <islomabdurakhmonov8@gmail.com>
2023-12-17 14:52:48 +0500

    routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './views/login/login.component';
-import { HomeComponent } from './views/home/home.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   	selector: 'app-root',
   	standalone: true,
   	imports: [
-  	  	CommonModule, 
-		RouterOutlet, 
-		LoginComponent,
-		HomeComponent,
+		CommonModule,
+		RouterOutlet,
+		RouterLink,
+		RouterLinkActive,
 	],
   	templateUrl: './app.component.html',
   	styleUrl: './app.component.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+];
