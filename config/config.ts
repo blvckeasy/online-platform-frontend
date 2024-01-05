1d585f724a7b83d45948e6d4a6e8d51e8510337b
blvckeasy <islomabdurakhmonov8@gmail.com>
2024-01-05 20:38:42 +0500

    updated

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL: string = `http://localhost:9000`
+export const BACKEND_URL: string = `https://online-platform-d9fc1103e0d0.herokuapp.com`
 export const BACKEND_URL_GRAPHQL: string = `${BACKEND_URL}/graphql`
 export const BACKEND_URL_REST_THUMBNAIL: string = `${BACKEND_URL}/thumbnail_url/`
 export const BACKEND_URL_REST_COURSE_VIDEO: string = `${BACKEND_URL}/api/v1/course-video/`
