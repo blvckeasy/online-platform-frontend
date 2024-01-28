711aafd09093fcc8482bd4e0db39c0a5201d73c1
blvckeasy <islomabdurakhmonov8@gmail.com>
2024-01-28 19:13:33 +0500

    updated

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,8 @@
-export const BACKEND_URL: string = `http://localhost:9000`
-export const BACKEND_WEBSOCKET_URL: string = `http://localhost:9000`
+// export const BACKEND_URL: string = `http://localhost:9000`
+// export const BACKEND_WEBSOCKET_URL: string = `http://localhost:9000`
 
-// export const BACKEND_URL: string = `https://online-platform-d9fc1103e0d0.herokuapp.com`;
-// export const BACKEND_WEBSOCKET_URL: string = `ws://online-platform-d9fc1103e0d0.herokuapp.com`;
+export const BACKEND_URL: string = `https://online-platform-d9fc1103e0d0.herokuapp.com`;
+export const BACKEND_WEBSOCKET_URL: string = `ws://online-platform-d9fc1103e0d0.herokuapp.com`;
 export const BACKEND_URL_GRAPHQL: string = `${BACKEND_URL}/graphql`;
 export const BACKEND_URL_REST_THUMBNAIL: string = `${BACKEND_URL}/thumbnail_url/`;
 export const BACKEND_URL_REST_COURSE_VIDEO: string = `${BACKEND_URL}/api/v1/course-video/`;
